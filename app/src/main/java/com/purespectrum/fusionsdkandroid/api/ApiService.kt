d38f940a6e65b84c2f082d504062482e02d64ee9
vinispectrum <vinicius.santana@purespectrum.com>
2025-08-08 19:54:42 -0300

    Add class type

--- app/src/main/java/com/purespectrum/fusionsdkandroid/api/ApiService.kt
+++ app/src/main/java/com/purespectrum/fusionsdkandroid/api/ApiService.kt
@@ -2,13 +2,14 @@ package com.purespectrum.fusionsdkandroid.api
 
 import com.purespectrum.fusionsdkandroid.model.ApiResponse
 import com.purespectrum.fusionsdkandroid.model.CurrencyResponse
+import com.purespectrum.fusionsdkandroid.model.Survey
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
 import retrofit2.http.QueryMap
 
 interface ApiService {
-    @GET("surveys/fusionMatch")
+    @GET("surveys/match")
     suspend fun getData(
         @Header("access-token") token: String,
         @Query("respondentId") respondentId: String,
@@ -19,9 +20,9 @@ interface ApiService {
         @Query("hashed_id") hashedId: String?,
         @Query("isSdk") isSdk: Boolean,
         @QueryMap(encoded = true) profileData: Map<String, String> = emptyMap()
-    ): retrofit2.Response<ApiResponse>
+    ): retrofit2.Response<ApiResponse<List<Survey>>>
 
-    @GET("sdkSettings")
+    @GET("sdkCurrency")
     suspend fun getCurrencyInfo(
         @Header("access-token") token: String
     ): retrofit2.Response<CurrencyResponse>
--- app/src/main/java/com/purespectrum/fusionsdkandroid/model/ApiResponse.kt
+++ app/src/main/java/com/purespectrum/fusionsdkandroid/model/ApiResponse.kt
@@ -1,6 +1,6 @@
 package com.purespectrum.fusionsdkandroid.model
 
-data class ApiResponse(
+data class ApiResponse<T>(
     val respondentId: String,
     val surveys: List<Survey>
 )
